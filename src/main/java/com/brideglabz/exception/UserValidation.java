4dea269e7c9a906ae80a4c4c11c71c5c77b438a9
usha-12 <usha12ahirwar@gmail.com>
2022-11-11 21:39:43 +0530

    User need to enter a valid email

--- src/main/java/com/brideglabz/exception/UserValidation.java
+++ src/main/java/com/brideglabz/exception/UserValidation.java
@@ -4,15 +4,19 @@ import java.util.regex.Pattern;
 
 public class UserValidation {
     public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+    public static final String EMAIL_ID="^[A-Za-z]{3,}[.]?[A-Za-z]{2,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
     public boolean checkFirstName(String firstName) {
         return (Pattern.matches(VALIDATE_NAME, firstName));
     }
     public Boolean checkLastName(String lastName) {
         return (Pattern.matches(VALIDATE_NAME,lastName));
     }
+
+    public Boolean checkEmailId(String emailId) {
+        return (Pattern.matches(EMAIL_ID,emailId));
+    }
 }
 /*As a User need to
-enter a valid Last
-Name
-- Last name starts with Cap and has
-minimum 3 characters*/
+enter a valid email - E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts (abc, bl
+& co) and 2 optional (xyz & in) with
+precise @ and . positions*/
--- src/test/java/com/brideglabz/exception/UserRegistration.java
+++ src/test/java/com/brideglabz/exception/UserRegistration.java
@@ -17,7 +17,7 @@ public class UserRegistration {
     @Test
     void given_first_name_should_returnTrue() {
         UserValidation validation = new UserValidation();
-        Boolean result = validation.checkFirstName("Aditya");
+        Boolean result = validation.checkFirstName("Usha");
         Assertions.assertEquals(result, true);
     }
     @Test
@@ -25,5 +25,11 @@ public class UserRegistration {
         UserValidation validation = new UserValidation();
         Boolean result1 = validation.checkLastName("Ahirwar");
         Assertions.assertEquals(result1, true);
+    }
+    @Test
+    void given_email_id_should_returnTrue() {
+        UserValidation validation = new UserValidation();
+        Boolean result2 = validation.checkEmailId("UshaAhirwar@gmail.com");
+        Assertions.assertEquals(result2, true);
     }
 }
