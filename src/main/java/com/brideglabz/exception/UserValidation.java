e6f688fac6659eec45be2ac6a5a73dcbaba423d9
usha-12 <usha12ahirwar@gmail.com>
2022-11-11 21:28:24 +0530

    user need to enter a valid first name

--- src/main/java/com/brideglabz/exception/UserValidation.java
+++ src/main/java/com/brideglabz/exception/UserValidation.java
@@ -1,2 +1,10 @@
-package com.brideglabz.exception;public class UserValidation {
+package com.brideglabz.exception;
+
+import java.util.regex.Pattern;
+
+public class UserValidation {
+    public static final String VALIDATE_NAME="^[A-z]+[a-z]{2,}$";
+    public boolean checkFirstName(String firstName) {
+        return (Pattern.matches(VALIDATE_NAME, firstName));
+    }
 }
